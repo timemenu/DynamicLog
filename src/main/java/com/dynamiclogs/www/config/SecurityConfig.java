f2c8daeefbb44222b53d93f6686a9345a16c2aa0
quickmenu <sweetmelody@hanmail.net>
2016-01-25 20:25:05 +0900

    스프링 시큐리티 jdbcAuthentication 추가

--- src/main/java/com/dynamiclogs/www/config/SecurityConfig.java
+++ src/main/java/com/dynamiclogs/www/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.dynamiclogs.www.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 
  * 시큐리티 설정 클래스입니다.
  *
- * com.dynamiclogs.www.config
- * SecurityConfig.java
+ * com.dynamiclogs.www.config SecurityConfig.java
  *
  * @author : quickmenu
  * @date : 2016. 1. 24.
@@ -18,35 +24,62 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  *
  */
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery(getUserQuery())
+			.authoritiesByUsernameQuery(getAuthoritiesQuery());
+	}
+
+	// 1. 유저네임 쿼리
+	private String getUserQuery() {
+		return "SELECT email AS username, password, TRUE AS enabled "
+				+ "FROM USER " 
+				+ "WHERE email = ?";
+	}
+
+	// 2. 권한관련 쿼리
+	private String getAuthoritiesQuery() {
+		return "SELECT email AS username, role "
+				+ "FROM USER "
+                + "WHERE email = ?";
+	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin@dynamiclog.net").password("1234").roles("USER");
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resource/**");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.headers().frameOptions().disable().and()
-			.csrf().disable().anonymous()
-		.and()
-			.authorizeRequests()
-			.antMatchers("/statics/**", "/signup", "/console/**")
-			.permitAll()
-			.anyRequest()
-			.authenticated()
-		.and()
-			.formLogin()
-			.loginPage("/login")
-			.loginProcessingUrl("/login/process")
-			.defaultSuccessUrl("/")
-			.permitAll()
-		.and()
-			.logout()
-			.logoutUrl("/logout")
-			.logoutSuccessUrl("/login")
-			.invalidateHttpSession(true)
-			.deleteCookies("JSESSIONID");
+		http.headers().frameOptions().disable()
+			.and().csrf().disable().anonymous()
+			.and()
+				.authorizeRequests()
+				.antMatchers("/statics/**", "/signup", "/console/**")
+				.permitAll().anyRequest().authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/login/process")
+				.defaultSuccessUrl("/")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login")
+				.invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID");
 	}
 }
---
+++ src/main/java/com/dynamiclogs/www/process/user/domain/User.java
@@ -0,0 +1,65 @@
+package com.dynamiclogs.www.process.user.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="u_seq")
+	private int uSeq;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "role")
+	private String role;
+
+	public int getuSeq() {
+		return uSeq;
+	}
+
+	public void setuSeq(int uSeq) {
+		this.uSeq = uSeq;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [" + (super.toString() != null ? "toString()=" + super.toString() : "") + "]";
+	}
+
+}
